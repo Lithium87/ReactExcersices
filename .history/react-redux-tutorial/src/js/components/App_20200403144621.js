86473182720694dd156f56fec46621d19a830979
Lithium87 <cibs@abv.bg>
2020-04-06 22:30:22 +0300

    Commit

---
+++ .history/react-redux-tutorial/src/js/components/App_20200403144621.js
@@ -0,0 +1,18 @@
+import React from "react";
+import List from "./List";
+import Form from './Form';
+
+const App = () => (
+  <React.Fragment>
+    <div>
+      <h2>Articles</h2>
+        <List />
+    </div>
+    <div>
+      <h2>Add a new article</h2>
+      <Form/>
+    </div>
+  </React.Fragment>
+);
+
+export default App;
