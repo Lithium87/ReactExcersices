6570e5a56e30bee108ef525338c18d4872e9ae34
Lithium87 <cibs@abv.bg>
2020-06-23 18:56:53 +0300

    Determine and Display Season

--- .history/seasons/src/SeasonDisplay_20200623183034.js
+++ .history/seasons/src/SeasonDisplay_20200623183034.js
@@ -1,16 +1,5 @@
 import React from "react";
 
-const seasonConfig = {
-  summer: {
-    text: "Lest's hit the beach!",
-    iconName: "sun",
-  },
-  winter: {
-    text: "Burr, it is chilly",
-    iconName: "snowflake",
-  },
-};
-
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter";
@@ -21,13 +10,12 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const { text, iconName } = seasonConfig[season];
+  const text =
+    season === "winter" ? "Burr, it is chilly" : "Lets hit the beach";
 
   return (
     <div>
-      <i className={`${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${iconName} icon`} />
     </div>
   );
 };
