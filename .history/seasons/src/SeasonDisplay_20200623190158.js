db08c8334d219ff6a400e56c0b8527be88bd0b4d
Lithium87 <cibs@abv.bg>
2020-06-23 19:17:47 +0300

    Add some styling

--- .history/seasons/src/SeasonDisplay_20200623190158.js
+++ .history/seasons/src/SeasonDisplay_20200623190158.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./SeasonDisplay.css";
 
 const seasonConfig = {
   summer: {
@@ -25,7 +24,7 @@ const SeasonDisplay = (props) => {
   const { text, iconName } = seasonConfig[season];
 
   return (
-    <div className={`season-display ${season}`}>
+    <div>
       <i className={`icon-left massive ${iconName} icon`} />
       <h1>{text}</h1>
       <i className={`icon-right massive ${iconName} icon`} />
