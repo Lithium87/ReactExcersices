c292df789108616bd76339bd4d9dbba26bd687dd
Lithium87 <cibs@abv.bg>
2020-06-30 23:51:25 +0300

    Final commit

---
+++ .history/songs/src/components/App_20200630214937.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SongList from "./SongList";
+
+const App = () => {
+  return (
+    <div className="ui container grid">
+      <div className="ui row">
+        <SongList />
+      </div>
+    </div>
+  );
+};
+
+export default App;
